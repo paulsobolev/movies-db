4ec944be168549f3b3d6d63ce1000adfcd9611c5
Paul Sobolev <cloud@paulsobolev.pro>
2017-02-18 01:56:15 +0500

    Add genres

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React from 'react'
 import {Segment, Header, Form, Rating, Button} from 'semantic-ui-react'
 import {generateYears} from '../../utils'
+import _genres from '../../store/constants/genres'
 
+const genres = _genres.map((g) => ({key: g, value: g, text: g}))
 const years = generateYears(30)
 
 function AddMovieForm() {
@@ -10,9 +12,8 @@ function AddMovieForm() {
       <Header as="h4">New movie</Header>
       <Form>
         <Form.Input fluid placeholder="Movie title" />
-        <Form.Select inline placeholder="Genres" options={years} multiple />
+        <Form.Select inline placeholder="Genres" options={genres} multiple />
         <Form.Select inline placeholder='Year' options={years} />
-
         <Form.Field inline>
           <label>Rating</label>
           <Rating
@@ -22,7 +23,6 @@ function AddMovieForm() {
             onRate={(e, data) => console.info("onRate", e, data)}
           />
         </Form.Field>
-
         <div className="clearfix">
           <Button primary type="submit" floated="right" icon="checkmark" content="Save" />
         </div>
